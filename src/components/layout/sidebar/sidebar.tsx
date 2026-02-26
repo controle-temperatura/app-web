09268306bd218ed3c0aa2640c3e6da5b94c46bd3
Guilherme Santos <guilhermesantosdev@outlook.com>
2026-02-26 09:51:38 -0300

    fix: removida a aba 'tabelas' da visualizacao do colaborador

--- src/components/layout/sidebar/sidebar.tsx
+++ src/components/layout/sidebar/sidebar.tsx
@@ -61,10 +61,10 @@ export function Sidebar() {
                         <SidebarMenuItem>
                             <SidebarItem icon={<LayoutDashboardIcon className="h-4 w-4" />} label="Dashboard" href="/dashboard" isActive={pathname === '/dashboard'} onClick={() => router.push("/dashboard")} />
                         </SidebarMenuItem>
-                        <SidebarMenuItem>
-                            <SidebarItem icon={<SheetIcon className="h-4 w-4" />} label="Tabelas" href="/tables" isActive={pathname === '/tables'} onClick={() => router.push("/tables")} />
-                        </SidebarMenuItem>
                         {(isAdmin || isAuditor) && (
+                            <SidebarMenuItem>
+                                <SidebarItem icon={<SheetIcon className="h-4 w-4" />} label="Tabelas" href="/tables" isActive={pathname === '/tables'} onClick={() => router.push("/tables")} />
+                            </SidebarMenuItem>
                             <SidebarMenuItem>
                                 <SidebarItem icon={<FileTextIcon className="h-4 w-4" />} label="Relatórios" href="/reports" isActive={pathname === '/reports'} onClick={() => router.push("/reports")} />
                             </SidebarMenuItem>
